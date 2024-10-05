98021282d1dbb9b52235706b23fc0537975dfdf1
Luana <luanaos2512@gmail.com>
2024-10-05 22:54:28 +0200

    First results

--- mymodels/main.py
+++ mymodels/main.py
@@ -1,7 +1,7 @@
 import torch.optim as optim 
 import torch.nn as nn 
 import sys
-sys.path.append('..\\U-2-Net\\')
+sys.path.append('../U-2-Net/')
 from model.u2net import U2NET
 from dataset import SegmentationDataset
 from torch.utils.data import DataLoader
@@ -21,7 +21,7 @@ def main():
 
     model = U2NET().to(device)
     print("Initializing weights...")
-    model.load_state_dict(torch.load('..\\U-2-Net\\saved_models\\u2net.pth'))
+    model.load_state_dict(torch.load('../U-2-Net/saved_models/u2net.pth'))
 
     criterion = nn.BCEWithLogitsLoss()
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
@@ -32,10 +32,10 @@ def main():
     dataloader_training=DataLoader(dataset_training, batch_size=4, shuffle=True)
 
     dataset_eval= SegmentationDataset(
-        images_dir=images_dir,
-        masks_dir=masks_dir,
+        images_dir="validation_images",
+        masks_dir="validation_masks",
     )
-    dataloader_eval=DataLoader(dataset_eval, batch_size=4, shuffle=True)
+    dataloader_eval=DataLoader(dataset_eval, batch_size=4, shuffle=False)
 
     print('Training model...')
 
@@ -48,7 +48,7 @@ def main():
             device=device
         )
         running_loss_eval=eval_model(
-            dataloader_training, 
+            dataloader_eval, 
             model=model, 
             criterion=criterion, 
             device=device
